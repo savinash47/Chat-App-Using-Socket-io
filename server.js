f25251bb14170e7b036bdf1ebf189f367ae85be2
Avinash <savinash47@gmail.com>
2015-11-29 19:57:03 -0800

    show timestamp

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var moment = require('moment');
+var now = moment();
+
+var currentTime = now.format('h:mm a');
 
 app.use(express.static(__dirname + '/public'));
 
@@ -10,14 +14,18 @@ io.on('connection', function (socket) {
 	console.log('connected');
 
 	socket.on('message', function (message){
+		
+
 		console.log('message sent is' + message.text);
 		//io.emit to send message even to the sender
 		//socket.broadcast.emit('message',message);
+		message.timestamp = moment().valueOf();
 		io.emit('message',message);
 	});
 
 	socket.emit('message', {
-		text: 'Welcome to fun'
+		text: 'Welcome to fun',
+		timestamp: moment().valueOf()
 	});
 });
 
