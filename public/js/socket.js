93bf10003ffd1efd7f4dbbfea7127bf9180e70fe
Avinash <savinash47@gmail.com>
2015-11-29 18:25:24 -0800

    send message using front end

--- public/js/socket.js
+++ public/js/socket.js
@@ -9,3 +9,18 @@ socket.on('message', function (message) {
 	console.log('new Message');
 	console.log(message.text);
 });
+
+//
+
+var $form = jQuery('#message-form');
+
+$form.on('submit', function(event){
+	event.preventDefault();
+
+	var $message = $form.find('input[name=messageBox]')
+	socket.emit('message',{
+		text: $message.val()
+	})
+
+	$message.val('');
+});
